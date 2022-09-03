7ee52fa12decbe5daf2666c90285bdb25cc1c119
medardo <bat.chevere@gmail.com>
2022-09-03 14:19:02 -0500

    desde mac

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PruebaComponent } from './prueba/prueba.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
-  declarations: [AppComponent, PruebaComponent],
+  declarations: [AppComponent, PruebaComponent, Test2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
